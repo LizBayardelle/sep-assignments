94e01bb23e18e3f6889beaf8e722e8bccd46db74
Liz Bayardelle <lizbayardelle@gmail.com>
2017-11-19 05:19:27 -0800

    Queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,22 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[@queue.count]
   end
 
   def enqueue(element)
+    @queue << element
+    @head = @queue[0]
+    @tail = element
   end
 
   def dequeue
+    @queue = @queue.drop(1)
+    @head = @queue[0]
+    @tail = @queue[@queue.count]
   end
 
   def empty?
+    @queue == []
   end
 end
